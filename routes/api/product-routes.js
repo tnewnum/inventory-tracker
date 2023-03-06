09e2c90deb5bdb0d528cfd1f99ae606d592ffa8c
Thomas Newnum <thomas.newnum@gmail.com>
2023-03-05 20:24:05 -0500

    post works

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,15 +36,16 @@ router.get('/:id', async (req, res) => {
 });
 
 // // create new product
-// router.post('/', (req, res) => {
-//   /* req.body should look like this...
-//     {
-//       product_name: "Basketball",
-//       price: 200.00,
-//       stock: 3,
-//       tagIds: [1, 2, 3, 4]
-//     }
-//   */
+router.post('/', async (req, res) => {
+    try {
+        const productData = await Product.create(req.body);
+        res.status(200).json(productData)
+      } 
+      catch (err) {
+        res.status(400).json(err);
+      }
+
+});
 //   Product.create(req.body)
 //     .then((product) => {
 //       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
