2035966b843ea7e60bd2b8893632140a03b5fd45
Thomas Newnum <thomas.newnum@gmail.com>
2023-03-06 08:12:25 -0500

    post route for tag works

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,10 +18,10 @@ router.get('/', async (req, res) => {
 // find a single tag by its `id`
 router.get('/:id', async (req, res) => {    
     try{
-        const productData = await Tag.findByPk(req.params.id, {
+        const tagData = await Tag.findByPk(req.params.id, {
          include: Product
         })
-        res.status(200).json(productData)
+        res.status(200).json(tagData)
     }
 
     catch (err) {
@@ -30,9 +30,17 @@ router.get('/:id', async (req, res) => {
     
 });
 
-// router.post('/', (req, res) => {
-//   // create a new tag
-// });
+// create a new tag
+router.post('/', async (req, res) => {
+    try {
+        const tagData = await Tag.create(req.body);
+        res.status(200).json(tagData)
+    } 
+    
+    catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 // router.put('/:id', (req, res) => {
 //   // update a tag's name by its `id` value
