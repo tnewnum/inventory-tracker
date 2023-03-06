e4de191d435b449f95a319438c3ce5845b6f9c6a
Thomas Newnum <thomas.newnum@gmail.com>
2023-03-06 07:24:13 -0500

    api/tags/ works

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// // The `/api/tags` endpoint
+// // Using the `/api/tags` endpoint
 
-// router.get('/', (req, res) => {
-//   // find all tags
-//   // be sure to include its associated Product data
-// });
+// find all tags
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: Product
+    });
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
 
 // router.get('/:id', (req, res) => {
 //   // find a single tag by its `id`
