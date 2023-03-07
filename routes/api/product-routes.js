b99f444db925c30b4a3e10003c550d60c456235b
Thomas Newnum <thomas.newnum@gmail.com>
2023-03-07 09:38:40 -0500

    error in product put fixed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -75,13 +75,15 @@ router.put('/:id', (req, res) => {
       id: req.params.id,
     },
   })
-    .then((product) => {
+    .then(() => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
     .then((productTags) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      console.log('===================')
+      console.log(req.body.tagIds)
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
@@ -104,7 +106,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(500).json(err);
     });
 });
